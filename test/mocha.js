ee30125f65a20aa100d8a09fa8ab20314ab8a116
Sema <jazz-dev@jazz-soft.net>
2021-08-15 00:54:37 -0400

    Updated dependencies

--- test/mocha.js
+++ test/mocha.js
@@ -10,7 +10,7 @@ global.__coverage__ = {};
 describe('In browser', function() {
   before(function() {
     return new Promise(function(resolve) {
-      JSDOM.fromFile('test/test.html', {
+      JSDOM.fromFile('test/demo.html', {
         resources: 'usable',
         runScripts: 'dangerously',
         beforeParse: function(window) {
